e4da4e9649c6cc0f64da5081937a0a20d48c8952
JJOJJORing <liverforest@gmail.com>
2019-10-30 15:00:41 +0900

    Added preview, publish, remove button and basic login using login view

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+
 ]
